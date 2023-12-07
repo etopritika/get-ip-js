d3e5ee62a68121b98ce68bff47d8154189378928
Dmitriy Prytyka <67103761+etopritika@users.noreply.github.com>
2023-12-08 00:14:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const textEl = document.querySelector("[data-ip]");
+
 const ip = async () => {
   try {
     const response = await fetch("https://api.db-ip.com/v2/free/self");
@@ -5,7 +7,7 @@ const ip = async () => {
       throw new Error("Network response was not ok.");
     }
     const data = await response.json();
-    alert(`Your IP address: ${data.ipAddress}`);
+    textEl.textContent = `Your IP address: ${data.ipAddress}`;
   } catch (error) {
     console.error("There has been a problem with your fetch operation:", error);
   }
